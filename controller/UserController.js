1621694190be0369cda9c8b249f2047d084f8a87
Julliane J. Tampus <jull@Jullianes-MacBook-Air.local>
2025-11-04 21:53:37 +0800

    added register route not tested

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,15 +1,64 @@
 const db = require('../database/DB')
+const bcrypt = require('bcrypt')
 
 
 class User{
 
 
-    add_user(){
+    async add_user(username, name, user_role, password){
+
+        try {
+            const salt = 20
+    
+            const hash_password = await bcrypt.hash(password, salt)
+    
+            const query = await db.query(`INSERT user_tbl(username, name, user_role, password) VALUES('${username}', '${name}', '${user_role}', '${hash_password}')`)
+    
+            console.log(query)
+            return true
         
+        } catch (error) {
+            console.log(error)   
+            return false 
+        }
     }
 
-    check_user(){
+    async check_user(username){
+
+        try {
+            const query = await db.query(`SELECT username FROM user_tbl WHERE username='${username}'`)
+
+            if(query.rowCount > 0) return false
+
+            return true
+            
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+
+    }
+
+    async login(username , password){
+
+        
+        try {
+            const query = await db.query(`SELECT username, password FROM user_tbl WHERE username='${username}'`)
+            
+            if(bcrypt.compare(password, query.rows[0]['password'])) return true
+
+            return false
+
+        } catch (error) {
+
+            console.log(error)
+            
+            return false
+            
+        }
 
     }
 
 }
+
+module.exports = User
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const router = express.Router()
-
+const UserController = require('../controller/UserController')
 
 router.post("/login", (req , res) =>{
+
 
 })
 
 
-router.post("/register", (req, res)=>{
-    
+router.post("/register", async (req, res)=>{
+    const reg_user = {
+        username : req.body.username,
+        name : req.body.name,
+        user_role : req.body.user_role,
+        password : req.body.password
+    }
+
+    const user = new UserController()
+
+
+    if(await user.add_user(reg_user.username, reg_user.name , reg_user.user_role, reg_user.password)){
+        return res.json({"message" : "successfully registered."}).status(200)
+    }
+
+    return res.json({"message" : "unable to register user"}).status(500)
+
+})
+
+
+router.get("/check_username/:username", (req ,res)=>{
+
+    //req.params.username
+
 })
 
 
